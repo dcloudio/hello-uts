52c49ade2c24eb9a964c6cf9544048f80445ac16
mahaifeng <mahaifeng@dcloud.io>
2024-07-02 15:30:23 +0800

    [array-buffer]消除告警

--- uni_modules/uts-tests/utssdk/TUInt8ClampedArray.js
+++ uni_modules/uts-tests/utssdk/TUInt8ClampedArray.js
@@ -64,7 +64,7 @@ export class TUint8ClampedArray {
 
   testEvery() {
     // const isBelowThreshold = (currentValue: number, index: number, array:Uint8ClampedArray): boolean => currentValue < 40;    
-    let result = new Uint8ClampedArray([12, 5, 8, 130, 44]).every((value: number, index: number, array:
+    let result = new Uint8ClampedArray([12, 5, 8, 130, 44]).every((value: number, _: number, _:
       Uint8ClampedArray): boolean => value < 40); // 
     expect(result).toEqual(false);
   }
@@ -89,29 +89,29 @@ export class TUint8ClampedArray {
   testFilter() {
     // const isBelowThreshold = (currentValue: number, index: number, _): boolean => currentValue >= 10;
 
-    let float32 = new Uint8ClampedArray([12, 5, 8, 44]).filter((value: number, index: number, array:
+    let float32 = new Uint8ClampedArray([12, 5, 8, 44]).filter((value: number, _: number, _:
       Uint8ClampedArray): boolean => value >= 10);
     expect(float32.toString()).toEqual("12,44");
   }
 
   find() {
     let float32 = new Uint8ClampedArray([4, 5, 8, 12]);
-    let res = float32.find((value: number, index: number, obj: Uint8ClampedArray): boolean => value > 5);
+    let res = float32.find((value: number, _: number, _: Uint8ClampedArray): boolean => value > 5);
     expect(res).toEqual(8);
   }
 
   findIndex() {
     let float32 = new Uint8ClampedArray([4, 6, 8, 12]);
-    let res = float32.findIndex((value: number, index: number, obj: Uint8ClampedArray): boolean => value > 100);
+    let res = float32.findIndex((value: number, _: number, _: Uint8ClampedArray): boolean => value > 100);
     expect(res).toEqual(-1);
 
     let ufloat32 = new Uint8ClampedArray([4, 6, 7, 120]);
-    res = ufloat32.findIndex((value: number, index: number, obj: Uint8ClampedArray): boolean => value > 100);
+    res = ufloat32.findIndex((value: number, _: number, _: Uint8ClampedArray): boolean => value > 100);
     expect(res).toEqual(3);
   }
 
   foreach() {
-    new Uint8ClampedArray([0, 1, 2, 3]).forEach((value: number, index: number, array: Uint8ClampedArray) => {
+    new Uint8ClampedArray([0, 1, 2, 3]).forEach((value: number, index: number, _: Uint8ClampedArray) => {
       console.log(`a[${index}] = ${value}`);
     });
   }
@@ -173,31 +173,31 @@ export class TUint8ClampedArray {
 
   map() {
     let numbers = new Uint8ClampedArray([1, 4, 9]);
-    let doubles = numbers.map((value: number, index: number, array: Uint8ClampedArray): number => value * 2);
+    let doubles = numbers.map((value: number, _: number, _: Uint8ClampedArray): number => value * 2);
     expect(numbers.toString()).toEqual("1,4,9");
     expect(doubles.toString()).toEqual("2,8,18");
   }
 
   reduce() {
     let total = new Uint8ClampedArray([0, 1, 2, 3]);
-    let res = total.reduce((accumulator: number, currentValue: number, currentIndex: number, array:
+    let res = total.reduce((accumulator: number, currentValue: number, _: number, _:
       Uint8ClampedArray): number => accumulator + currentValue);
     expect(res).toEqual(6);
 
     total = new Uint8ClampedArray([0, 1, 2, 3]);
-    res = total.reduce((accumulator: number, currentValue: number, currentIndex: number, array: Uint8ClampedArray):
+    res = total.reduce((accumulator: number, currentValue: number, _: number, _: Uint8ClampedArray):
       number => accumulator + currentValue, 8);
     expect(res).toEqual(14);
   }
 
   reduceRight() {
     let total = new Uint8ClampedArray([0, 1, 2, 3]);
-    let res = total.reduceRight((accumulator: number, currentValue: number, currentIndex: number, array:
+    let res = total.reduceRight((accumulator: number, currentValue: number, _: number, _:
       Uint8ClampedArray): number => accumulator + currentValue);
     expect(res).toEqual(6);
 
     total = new Uint8ClampedArray([0, 1, 2, 3]);
-    res = total.reduceRight((accumulator: number, currentValue: number, currentIndex: number, array:
+    res = total.reduceRight((accumulator: number, currentValue: number, _: number, _:
       Uint8ClampedArray): number => accumulator + currentValue, 8);
     expect(res).toEqual(14);
   }
