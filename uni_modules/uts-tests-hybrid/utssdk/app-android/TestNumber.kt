86545649ffe526b2f9878d3345163deab9b31eab
duqingquan <duqingquan@dcloud.io>
2025-03-31 17:54:42 +0800

    增加混编代码示例Android版本

--- uni_modules/uts-tests-hybrid/utssdk/app-android/TestNumber.kt
+++ uni_modules/uts-tests-hybrid/utssdk/app-android/TestNumber.kt
@@ -1,8 +1,11 @@
-import Foundation
-import DCloudUTSFoundation
+package uts.sdk.modules.utsTestsHybrid
 
-class TestNumber {
-    static func testAll() {
+import io.dcloud.uts.*
+
+
+object TestNumber{
+    
+    fun testAll() {
         testToFixed()
         testIsFinite()
         testIsInteger()
@@ -14,78 +17,81 @@ class TestNumber {
         testFrom()
     }
     
-    static func testToFixed() {
+    @Suppress("DEPRECATION","IMPLICIT_BOXING_IN_IDENTITY_EQUALS")
+    fun testToFixed() {
         // #TEST Number.toFixed
-        let financial = { (x: NSNumber) in
+        fun financial(x: Number): String {
             return x.toFixed(2)
         }
-        
-        console.log(financial(NSNumber(value: 123.456)))
+        console.log(financial(123.456))
         // expected output: "123.46"
-        console.log(financial(NSNumber(value: 0.004)));
+        console.log(financial(0.004))
         // expected output: "0.00"
-        
-        let num: NSNumber = 3.1415926
-        console.log(num.toFixed(2))
-        //expected output: "3.14"
         // #END
     }
     
-    static func testIsFinite() {
+    fun testIsFinite() {
         // #TEST Number.isFinite
-        console.log(NSNumber.isFinite(1000 / 1))
+        console.log(isFinite(1000))
         //expected output:  true
-        console.log(NSNumber.isFinite(910))
+        console.log(isFinite(910))
         //expected output:  true
-        console.log(NSNumber.isFinite(Double.infinity))
+        console.log(isFinite(0))
         //expected output:  false
         // #END
     }
     
-    static func testIsInteger() {
+    fun testIsInteger() {
         // #TEST Number.isInteger
-        console.log(NSNumber.isInteger(12))
+        console.log(UTSNumber.isInteger(12))
         //expected output:  true
-        console.log(NSNumber.isInteger(12.01))
+        console.log(UTSNumber.isInteger(12.01))
         //expected output:  false
-        console.log(NSNumber.isInteger(-213123112.01))
+        console.log(UTSNumber.isInteger(-213123112.01))
         //expected output:  false
-        console.log(NSNumber.isInteger(-213123112))
+        console.log(UTSNumber.isInteger(-213123112))
         //expected output:  true
         // #END
     }
     
-    static func testIsNaN() {
+    
+    fun testIsNaN() {
         // #TEST Number.isNaN
         console.log(isNaN(0))
         //expected output: false
-        console.log(NSNumber.isNaN(0))
-        // expected output: false
-        let obj: UTSJSONObject? = JSON.parseObject("{\"a\":1}")
-        let aNumber = obj?.getNumber("a") ?? 0
-        console.log(NSNumber.isNaN(aNumber))
-        //expected output:  false
-        console.log(NSNumber.isNaN(11))
-        //expected output:  false
-        console.log(NSNumber.isNaN( 1 / 0))
-        //expected output:  false
+        var aj2 = JSON.parse("{\"a\":1}") as UTSJSONObject
+        var aNumber = aj2["a"] as Number
+        console.log(isNaN(aNumber))
+        //expected output: false
+        console.log(UTSNumber.isNaN(aNumber))
+        //expected output: false
+        console.log(UTSNumber.isNaN(11))
+        //expected output: false
+        console.log(UTSNumber.isNaN(null))
+        //expected output: false
+        console.log(UTSNumber.isNaN((1 as Number) / 0))
+        //expected output: false
+        
         // #END
     }
     
-    static func testToPrecision() {
+    
+    fun testToPrecision() {
         // #TEST Number.toPrecision
         console.log(123.456.toPrecision(4))
         //expected output: "123.5"
-        console.log((0.004).toPrecision(4))
+        console.log(0.004.toPrecision(4))
         //expected output:  "0.004000"
         // #END
     }
     
-    static func testToString() {
+    fun testToString() {
         // #TEST Number.toString
-        console.log(10.toString())
-        //expected output: "10"
-        console.log(17.2.toString())
+        console.log(10.toString(10))
+        //expected output:  "10"
+        console.log(17.toString(10))
+        //expected output:  "17"
+        console.log(17.2.toString(10))
         //expected output:  "17.2"
         console.log(6.toString(2))
         //expected output:  "110"
@@ -104,7 +110,7 @@ class TestNumber {
         // #END
     }
     
-    static func testValueOf() {
+    fun testValueOf() {
         // #TEST Number.valueOf
         console.log(10.valueOf())
         //expected output: 10
@@ -115,24 +121,27 @@ class TestNumber {
         // #END
     }
     
-    static func testToInt() {
-        // #TEST Number.toInt
-        let a: NSNumber = 12
-        console.log(a.toInt())
-        //expected output: 12
+    fun testToInt() {
+      // #TEST Number.toInt
+      var a: Number = 12
+      console.log(a.toInt())
+      //expected output: 12
         
-        let b: NSNumber = 2147483648
-        console.log(b.toInt())
-        //expected output:  -2147483648
-        // #END
+      var b: Number = 2147483648
+      console.log(b.toInt())
+      //expected output:  -2147483648
+      // #END
     }
     
-    static func testFrom() {
-        // #TEST Number.toInt
-        let a: Int = 12
-        let b = NSNumber.from(a)
-        console.log(b)
-        //expected output: 12
-        // #END
+    fun testFrom() {
+      // #TEST Number.toInt
+      var a: Int = 12
+      var b = UTSNumber.from(a)
+      console.log(b)
+      //expected output: 12
+      // #END
     }
+    
+    
+  
 }
